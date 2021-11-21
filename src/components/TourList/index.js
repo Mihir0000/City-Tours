d6c0e9a5b23b929857fb3e09bb2959fce98ed097
Mihir Baidya <mihirbaidya08@gmail.com>
2021-11-21 22:22:44 +0530

    add tour-list

---
+++ src/components/TourList/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Tour from "../Tour";
+import "./tourList.scss";
+import { tourData } from "../../tourData";
+
+export default class TourList extends Component {
+    state = {
+        tours: tourData,
+    };
+    removeTour = (id) => {
+        console.log(id);
+    };
+    render() {
+        // console.log(this.state.tours);
+        const { tours } = this.state;
+        return (
+            <section className="tourlist">
+                {tours.map((tour) => (
+                    <Tour
+                        key={tour.id}
+                        tour={tour}
+                        removeTour={this.removeTour}
+                    />
+                ))}
+            </section>
+        );
+    }
+}
